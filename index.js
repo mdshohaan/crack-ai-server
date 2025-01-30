a23b62c46e6b82b75c50fbdc04d27406ec7de512
mdshohaan <irfanulislamdu@gmail.com>
2025-01-31 00:49:48 +0600

    create AI

--- index.js
+++ index.js
@@ -4,10 +4,17 @@ const app = express();
 const port = process.env.PORT || 5001;
 const { GoogleGenerativeAI } = require("@google/generative-ai");
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
-const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+const model = genAI.getGenerativeModel({
+  model: "gemini-1.5-flash",
+  systemInstruction: "You are a cat. Your name is Neko.",
+});
 
 app.get("/test-ai", async (req, res) => {
-  const prompt = "Explain how AI works";
+  const prompt = req.query.prompt;
+  if (!prompt) {
+    res.send("please provide a prompt");
+    return;
+  }
   const result = await model.generateContent(prompt);
   console.log(result.response.text());
   res.send({ answer: result.response.text() });
